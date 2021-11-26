768ebf1e8216323b937873d10b5d9ac5fdad781c
ghettojezuz <rebim@yandex.ru>
2021-11-26 21:03:21 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
-// === Swiper ===
-const upcomingSlider = new Swiper('.swiper-upcoming', {
-    loop: true,
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.swiper-upcoming-button-next',
-        prevEl: '.swiper-upcoming-button-prev',
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 10
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 12
-        },
-        1280: {
-            slidesPerView: 4,
-            spaceBetween: 29
-        }
+// === Header mobile menu ===
+const menu = document.getElementById('menu');
+const menuButton = document.getElementById('menu-button');
+
+function toggleMenu() {
+    if (!menu.classList.contains('transition')) {
+        menu.classList.add('transition');
     }
-});
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        menuButton.classList.remove('active');
+    } else {
+        menu.classList.add('active');
+        menuButton.classList.add('active');
+    }
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+
+// === Swiper ===
+const swipersUpcomingClass = '.swiper-upcoming';
+const swipersUpcoming = document.querySelectorAll(swipersUpcomingClass);
+if (swipersUpcoming.length > 0) {
+    const upcomingSlider = new Swiper(swipersUpcomingClass, {
+        loop: true,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.swiper-upcoming-button-next',
+            prevEl: '.swiper-upcoming-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 12
+            },
+            1280: {
+                slidesPerView: 4,
+                spaceBetween: 29
+            }
+        }
+    });
+}
+
